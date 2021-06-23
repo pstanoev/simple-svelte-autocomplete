d6d23aff90cb79500c8bbadaa4abf069ff6ba5cb
Éloi Rivard <azmeuk@gmail.com>
2021-06-23 23:57:57 +0200

    demo tabs

--- src/App.svelte
+++ src/App.svelte
@@ -13,32 +13,77 @@ import AsyncExample from "./AsyncExample.svelte";
 import MatchingStrategyExample from "./MatchingStrategyExample.svelte";
 import AsyncGeneratorExample from "./AsyncGeneratorExample.svelte";
 import AsyncPreloadedExample from "./AsyncPreloadedExample.svelte";
+
+function showTab(e, tab) {
+    for(let el of document.getElementsByClassName("tab")) {
+        el.classList.remove("is-active");
+    }
+    e.target.parentElement.classList.add("is-active");
+
+    for(let el of document.getElementsByClassName("tab-content")) {
+        el.classList.remove('is-active');
+    }
+    for(let el of document.getElementsByClassName(tab)) {
+        el.classList.add('is-active');
+    }
+}
+
 </script>
 
 <svelte:head>
   {@html solarized}
 </svelte:head>
 
+<style>
+    .tab-content{
+        display:none;
+    }
+    .tab-content.is-active{
+        display:block;
+    }
+    h1.title a{
+        float: right;
+    }
+</style>
+
 <section class="section">
     <div class="container content">
-        <h1>Svelte Simple Autocomplete Demo</h1>
-
-        <p>
-            Github:
-            <a href="https://github.com/pstanoev/simple-svelte-autocomplete">
-                https://github.com/pstanoev/simple-svelte-autocomplete
+        <h1 class="title">
+            <a href="https://github.com/pstanoev/simple-svelte-autocomplete" class="icon">
+                <i class="fab fa-github"></i>
             </a>
-        </p>
+            Svelte Simple Autocomplete Demo
+        </h1>
 
-        <SimpleExample />
-        <LockedExample />
-        <AdvancedExample />
-        <MultipleExample />
-        <CustomizationExample />
-        <ActivityIndicatorExample />
-        <AsyncExample />
-        <MatchingStrategyExample />
-        <AsyncGeneratorExample />
+        <div class="tabs is-centered">
+            <ul>
+                <li on:click={e => showTab(e, "simple")} class="tab is-active"><a>Basic usage</a></li>
+                <li on:click={e => showTab(e, "customization")} class="tab"><a>Customization</a></li>
+                <li on:click={e => showTab(e, "async")} class="tab"><a>Async</a></li>
+                <li on:click={e => showTab(e, "advanced")} class="tab"><a>Advanced usage</a></li>
+            </ul>
+        </div>
+
+        <div class="tab-content simple is-active">
+            <SimpleExample />
+            <LockedExample />
+            <AdvancedExample />
+            <MultipleExample />
+        </div>
+
+        <div class="tab-content customization">
+            <CustomizationExample />
+        </div>
+
+        <div class="tab-content async">
+            <AsyncExample />
+            <ActivityIndicatorExample />
+            <AsyncGeneratorExample />
         <AsyncPreloadedExample />
+        </div>
+
+        <div class="tab-content advanced">
+            <MatchingStrategyExample />
+        </div>
     </div>
 </section>
